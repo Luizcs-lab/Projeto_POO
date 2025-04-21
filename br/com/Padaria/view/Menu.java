5a1cf7c99bcff7a1104a52b6e2319e00ab19c80f
cesar luiz da silva <cesarluizdasilva10@gmail.com>
2025-04-21 17:10:14 -0300

    Interface gráfica para sistema de padaria

---
+++ br/com/Padaria/view/Menu.java
@@ -0,0 +1,11 @@
+package br.com.Padaria.view;
+import javax.swing.JFrame;
+public class Menu {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("Padaria");
+    janela.setSize(600,600);
+    janela.setVisible(true);
+    
+    }
+    
+}
